e7c252f83b9f1d224d7d6738d273b6538f07f57d
Leon <Leon@LEONVO>
2017-05-13 07:25:06 +0800

    增加一点架构

---
+++ 发射小火箭/Model/View.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace 发射小火箭.Model
+{
+    /// <summary>
+    /// 视图基类
+    /// </summary>
+    class View
+    {
+        /// <summary>
+        /// 世界的尺寸
+        /// </summary>
+        private readonly Size in_WorldSize;
+        /// <summary>
+        /// 世界的地面高度
+        /// </summary>
+        private readonly int GroundTop;
+    }
+}
---
+++ 发射小火箭/Model/World.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 发射小火箭
+{
+    /// <summary>
+    /// 世界
+    /// </summary>
+    class World
+    {
+
+    }
+}
