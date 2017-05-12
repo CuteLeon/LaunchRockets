5b56bd587c28cfef226233d9ab31eade939ec338
Leon <Leon@LEONVO>
2017-05-13 07:17:52 +0800

    开始阶段

---
+++ 发射小火箭/GameForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 发射小火箭
+{
+    public partial class GameForm : Form
+    {
+        /// <summary>
+        /// 记录小火箭列表
+        /// </summary>
+        Rockets RocketList;
+
+        public GameForm()
+        {
+            InitializeComponent();
+            this.SetBounds(0, 0, Screen.FromHandle(this.Handle).Bounds.Width, Screen.FromHandle(this.Handle).Bounds.Height);
+            RocketList=new Rockets(this.ClientSize);
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+            Debug.Print( RocketList.ToString());
+        }
+    }
+}
---
+++ 发射小火箭/IMove.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 发射小火箭
+{
+    /// <summary>
+    /// 小火箭移动动作
+    /// </summary>
+    interface IMove
+    {
+        void Move();
+    }
+}
