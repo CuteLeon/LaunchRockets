9e9d24b5c94faf2e20e179643a0f6f536d9fc199
Leon <Leon@LEONVO>
2017-05-19 18:39:08 +0800

    允许修改帧间时间间隔

--- 发射小火箭/GameForm.Designer.cs
+++ 发射小火箭/GameForm.Designer.cs
@@ -30,10 +30,12 @@
         {
             this.components = new System.ComponentModel.Container();
             this.GameEngine = new System.Windows.Forms.Timer(this.components);
-            this.ShowPath = new System.Windows.Forms.CheckBox();
-            this.ShowPoints = new System.Windows.Forms.CheckBox();
-            this.ShowElementCount = new System.Windows.Forms.CheckBox();
             this.ShowRocketCount = new System.Windows.Forms.CheckBox();
+            this.ShowElementCount = new System.Windows.Forms.CheckBox();
+            this.ShowPoints = new System.Windows.Forms.CheckBox();
+            this.ShowPath = new System.Windows.Forms.CheckBox();
+            this.FPSTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.FPSTrackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // GameEngine
@@ -41,31 +43,18 @@
             this.GameEngine.Enabled = true;
             this.GameEngine.Tick += new System.EventHandler(this.GameEngine_Tick);
             // 
-            // ShowPath
+            // ShowRocketCount
             // 
-            this.ShowPath.AutoSize = true;
-            this.ShowPath.BackColor = System.Drawing.Color.Transparent;
-            this.ShowPath.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.ShowPath.ForeColor = System.Drawing.Color.White;
-            this.ShowPath.Location = new System.Drawing.Point(12, 12);
-            this.ShowPath.Name = "ShowPath";
-            this.ShowPath.Size = new System.Drawing.Size(125, 25);
-            this.ShowPath.TabIndex = 0;
-            this.ShowPath.Text = "显示飞行路径";
-            this.ShowPath.UseVisualStyleBackColor = false;
-            // 
-            // ShowPoints
-            // 
-            this.ShowPoints.AutoSize = true;
-            this.ShowPoints.BackColor = System.Drawing.Color.Transparent;
-            this.ShowPoints.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.ShowPoints.ForeColor = System.Drawing.Color.White;
-            this.ShowPoints.Location = new System.Drawing.Point(12, 43);
-            this.ShowPoints.Name = "ShowPoints";
-            this.ShowPoints.Size = new System.Drawing.Size(125, 25);
-            this.ShowPoints.TabIndex = 1;
-            this.ShowPoints.Text = "显示飞行坐标";
-            this.ShowPoints.UseVisualStyleBackColor = false;
+            this.ShowRocketCount.AutoSize = true;
+            this.ShowRocketCount.BackColor = System.Drawing.Color.Transparent;
+            this.ShowRocketCount.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ShowRocketCount.ForeColor = System.Drawing.Color.White;
+            this.ShowRocketCount.Location = new System.Drawing.Point(62, 114);
+            this.ShowRocketCount.Name = "ShowRocketCount";
+            this.ShowRocketCount.Size = new System.Drawing.Size(125, 25);
+            this.ShowRocketCount.TabIndex = 8;
+            this.ShowRocketCount.Text = "显示火箭计数";
+            this.ShowRocketCount.UseVisualStyleBackColor = false;
             // 
             // ShowElementCount
             // 
@@ -73,25 +62,52 @@
             this.ShowElementCount.BackColor = System.Drawing.Color.Transparent;
             this.ShowElementCount.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.ShowElementCount.ForeColor = System.Drawing.Color.White;
-            this.ShowElementCount.Location = new System.Drawing.Point(12, 74);
+            this.ShowElementCount.Location = new System.Drawing.Point(62, 85);
             this.ShowElementCount.Name = "ShowElementCount";
             this.ShowElementCount.Size = new System.Drawing.Size(125, 25);
-            this.ShowElementCount.TabIndex = 2;
+            this.ShowElementCount.TabIndex = 7;
             this.ShowElementCount.Text = "显示元素计数";
             this.ShowElementCount.UseVisualStyleBackColor = false;
             // 
-            // ShowRocketCount
+            // ShowPoints
             // 
-            this.ShowRocketCount.AutoSize = true;
-            this.ShowRocketCount.BackColor = System.Drawing.Color.Transparent;
-            this.ShowRocketCount.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.ShowRocketCount.ForeColor = System.Drawing.Color.White;
-            this.ShowRocketCount.Location = new System.Drawing.Point(12, 105);
-            this.ShowRocketCount.Name = "ShowRocketCount";
-            this.ShowRocketCount.Size = new System.Drawing.Size(125, 25);
-            this.ShowRocketCount.TabIndex = 3;
-            this.ShowRocketCount.Text = "显示火箭计数";
-            this.ShowRocketCount.UseVisualStyleBackColor = false;
+            this.ShowPoints.AutoSize = true;
+            this.ShowPoints.BackColor = System.Drawing.Color.Transparent;
+            this.ShowPoints.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ShowPoints.ForeColor = System.Drawing.Color.White;
+            this.ShowPoints.Location = new System.Drawing.Point(62, 56);
+            this.ShowPoints.Name = "ShowPoints";
+            this.ShowPoints.Size = new System.Drawing.Size(125, 25);
+            this.ShowPoints.TabIndex = 6;
+            this.ShowPoints.Text = "显示飞行坐标";
+            this.ShowPoints.UseVisualStyleBackColor = false;
+            // 
+            // ShowPath
+            // 
+            this.ShowPath.AutoSize = true;
+            this.ShowPath.BackColor = System.Drawing.Color.Transparent;
+            this.ShowPath.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ShowPath.ForeColor = System.Drawing.Color.White;
+            this.ShowPath.Location = new System.Drawing.Point(62, 27);
+            this.ShowPath.Name = "ShowPath";
+            this.ShowPath.Size = new System.Drawing.Size(125, 25);
+            this.ShowPath.TabIndex = 5;
+            this.ShowPath.Text = "显示飞行路径";
+            this.ShowPath.UseVisualStyleBackColor = false;
+            // 
+            // FPSTrackBar
+            // 
+            this.FPSTrackBar.Location = new System.Drawing.Point(11, 30);
+            this.FPSTrackBar.Maximum = 200;
+            this.FPSTrackBar.Minimum = 10;
+            this.FPSTrackBar.Name = "FPSTrackBar";
+            this.FPSTrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.FPSTrackBar.Size = new System.Drawing.Size(45, 104);
+            this.FPSTrackBar.TabIndex = 9;
+            this.FPSTrackBar.TickFrequency = 10;
+            this.FPSTrackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.FPSTrackBar.Value = 50;
+            this.FPSTrackBar.Scroll += new System.EventHandler(this.FPSTrackBar_Scroll);
             // 
             // GameForm
             // 
@@ -99,6 +115,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(693, 506);
+            this.Controls.Add(this.FPSTrackBar);
             this.Controls.Add(this.ShowRocketCount);
             this.Controls.Add(this.ShowElementCount);
             this.Controls.Add(this.ShowPoints);
@@ -111,6 +128,7 @@
             this.Text = "发射小火箭——[发呆专用系列]";
             this.Load += new System.EventHandler(this.GameForm_Load);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.GameForm_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.FPSTrackBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,10 +137,11 @@
         #endregion
 
         private System.Windows.Forms.Timer GameEngine;
-        public System.Windows.Forms.CheckBox ShowPath;
-        public System.Windows.Forms.CheckBox ShowPoints;
-        public System.Windows.Forms.CheckBox ShowElementCount;
         public System.Windows.Forms.CheckBox ShowRocketCount;
+        public System.Windows.Forms.CheckBox ShowElementCount;
+        public System.Windows.Forms.CheckBox ShowPoints;
+        public System.Windows.Forms.CheckBox ShowPath;
+        private System.Windows.Forms.TrackBar FPSTrackBar;
     }
 }
 
--- 发射小火箭/GameForm.cs
+++ 发射小火箭/GameForm.cs
@@ -25,6 +25,11 @@ namespace 发射小火箭
         {
             MainForm = this;
             InitializeComponent();
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.Selectable, true);
             this.Icon = (Icon)Resources.RocketResource.LaunchRocket;
             this.SetBounds(0, 0, Screen.FromHandle(this.Handle).Bounds.Width, Screen.FromHandle(this.Handle).Bounds.Height);
             RocketList=new Rockets(this.ClientSize);
@@ -44,10 +49,17 @@ namespace 发射小火箭
 
         private void GameEngine_Tick(object sender, EventArgs e)
         {
-            //return;
             this.BackgroundImage.Dispose();
             this.BackgroundImage = BitmapController.MergeBitmaps(GameMap.UpdateMap(), RocketList.UpdateRockets());
+            //this.CreateGraphics().Clear(Color.Black);
+            //this.CreateGraphics().DrawImageUnscaled(GameMap .UpdateMap (),0,0);
+            //this.CreateGraphics().DrawImageUnscaled(RocketList.UpdateRockets (), 0, 0);
             GC.Collect();
+        }
+
+        private void FPSTrackBar_Scroll(object sender, EventArgs e)
+        {
+            GameEngine.Interval = FPSTrackBar.Value;
         }
     }
 }
